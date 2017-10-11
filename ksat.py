17cb68cafb83a2d835d5b0c7f0ac6dfd344d475f
Servss <A01371719@itesm.mx>
2017-10-10 20:23:50 -0500

    Transformación CNF a 3-SAT

--- ksat.py
+++ ksat.py
@@ -1,18 +1,36 @@
 def sat_creator(variables, clause_type):
-    global dummy_number
+    global dummy_number 
     if clause_type == 1:
         #Beginning clause
+        print(variables[0], variables[1], dummy_number)
+        dummy_number *= -1
+        for i in range(2, len(variables)):
+            print(dummy_number, variables[i], end=' ')
+            dummy_number *= -1
+            dummy_number += 1
+            print(dummy_number) 
+            dummy_number *= -1           
+
     elif clause_type == 2:
-        #Middle clause
+        for i in range(len(variables)):
+            print(dummy_number, variables[i], end=' ')
+            dummy_number *= -1
+            dummy_number += 1
+            print(dummy_number)
+            dummy_number *= -1
+
     elif clause_type == 3:
         #Final clause
+
+        print(dummy_number, variables[-2], variables[-1])      
 
 dummy_number = 0
 
 def main():
+    global dummy_number
     try:
         header = True
-        counter = 0
+        counter = 1
         while True:
             line = input()
             if line[0] == "c":
@@ -22,19 +40,19 @@ def main():
                 if header:
                     variables = int(values[2])
                     clauses = int(values[3])
-                    print(variables, clauses)
+                    dummy_number = variables + 1
+                    #print(variables, clauses, dummy_number)
                     header = False
                 else:
-                    if counter == 0:
+                    if counter == 1:
                         #Beginning clause
                         clause_type = 1
-                    elif counter == (clauses - 1):
+                    elif counter == clauses:
                         #Final clause
                         clause_type = 3
                     else:
                         #Middle clause
                         clause_type = 2
-                    
                     #Removes last cero
                     values.pop()
 
